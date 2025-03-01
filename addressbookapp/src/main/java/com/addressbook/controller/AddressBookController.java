b8ec3749464fe29d327f49179e2ed638e5f091dd
SanketPatel77 <sanketpatel4872@gmail.com>
2025-03-01 15:11:20 +0530

    [Sanket] Add . Introduction to service layer.

--- addressbookapp/src/main/java/com/addressbook/controller/AddressBookController.java
+++ addressbookapp/src/main/java/com/addressbook/controller/AddressBookController.java
@@ -1,14 +1,27 @@
 package com.addressbook.controller;
 
+import com.addressbook.dto.AddressBookDTO;
+import com.addressbook.dto.ResponseDTO;
+import com.addressbook.service.AddressBookService;
+import com.addressbook.service.AddressBookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/addressbook")
 public class AddressBookController {
+
+    private AddressBookServiceImpl addressBookService;
+
+    @Autowired
+    public AddressBookController(AddressBookServiceImpl addressBookService) {
+        this.addressBookService = addressBookService;
+    }
+
     // testing application is working or not
     @GetMapping("/test")
-    public ResponseEntity<String> test(){
+    public ResponseEntity<String> test() {
         return ResponseEntity.ok("Connected Successfully");
     }
 }
---
+++ addressbookapp/src/main/java/com/addressbook/service/AddressBookServiceImpl.java
@@ -0,0 +1,8 @@
+package com.addressbook.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressBookServiceImpl implements AddressBookService{
+
+}
